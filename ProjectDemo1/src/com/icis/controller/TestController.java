adec7cf8b6c3f7ec1299032b429483da4c1b1d62
shixian <13521348721@163.com>
2020-12-14 17:05:19 +0800

    修改了Controller

--- ProjectDemo1/src/com/icis/controller/TestController.java
+++ ProjectDemo1/src/com/icis/controller/TestController.java
@@ -4,5 +4,7 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("哈哈哈");
+		  System.out.println("Hello world");
+        System.out.println("哈哈哈");
     }
 }
